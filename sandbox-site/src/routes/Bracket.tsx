84c6a61c98dee4df4a899e230c899a100a1cb5d7
Austin <austini@crowdhubapps.com>
2021-08-15 14:11:24 -0700

    Refactoring bracket nodes

--- sandbox-site/src/routes/Bracket.tsx
+++ sandbox-site/src/routes/Bracket.tsx
@@ -1,13 +1,18 @@
 import React from "react";
+import BracketNode from "./BracketNode";
 import "./Bracket.scss";
 
-class BracketNode {
+
+class BracketNodeConstructor {
     // eslint-disable-line no-unused-vars
+    id!: number;
+    parent!: number;
     roundName!: string;
     left!: any;
     right!: any;
     leftEntrant: string;
     rightEntrant: string;
+
 
     constructor() {
         this.roundName = "";
@@ -22,19 +27,40 @@ type BracketProps = {
     entries: number
 }
 
+const alphabet = [
+    "Grand Finals",
+    "Semifinal A",
+    "Semifinal B",
+    "Quarterfinal A",
+    "Quarterfinal B",
+    "Quarterfinal C",
+    "Quarterfinal D",
+    "Play-in A",
+    "Play-in B",
+    "Play-in C",
+    "Play-in D",
+    "Play-in E",
+    "Play-in F",
+    "Play-in G",
+    "Play-in H",
+    "Play-in I",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+];
+
 export class Bracket extends React.Component<BracketProps>{ // eslint-disable-line no-unused-vars
     public render(): JSX.Element {
         return (
             <div className="bracket">
                 {bracketBuild(this.props.entries).map(function (node, index) {
-                    return (
-                    <div key={index} className="bracket_node">
-                        <div className="title">{node.roundName}</div>
-                        <input />
-                        <div className="node_seed">{node.left ? `Winner of ${node.left}` : ' '}</div>
-                        <input />
-                        <div className="node_seed">{node.right ? `Winner of ${node.right}` : ' '}</div>
-                    </div>);
+                    return <BracketNode key={node.id} id={node.id} left={node.left} right={node.right} roundName={node.roundName} leftEntrant={node.leftEntrant} rightEntrant={node.rightEntrant} />
                 })}
             </div>
         );
@@ -44,44 +70,19 @@ export class Bracket extends React.Component<BracketProps>{ // eslint-disable-li
 export default Bracket; // eslint-disable-line no-unused-vars
 
 const bracketBuild = (entries: number) => {
-    const alphabet = [
-        "Grand Finals",
-        "Semifinal A",
-        "Semifinal B",
-        "Quarterfinal A",
-        "Quarterfinal B",
-        "Quarterfinal C",
-        "Quarterfinal D",
-        "Play-in A",
-        "Play-in B",
-        "Play-in C",
-        "Play-in D",
-        "Play-in E",
-        "Play-in F",
-        "Play-in G",
-        "Play-in H",
-        "Play-in I",
-        "R",
-        "S",
-        "T",
-        "U",
-        "V",
-        "W",
-        "X",
-        "Y",
-        "Z",
-    ];
     let nodesArray = [];
     let nodesNeeded = entries - 1;
     let nodesCreated = 0;
     while (nodesCreated < nodesNeeded) {
         nodesCreated++;
-        let node = new BracketNode();
+        let node = new BracketNodeConstructor();
+        node.id = nodesCreated;
         node.roundName = alphabet[nodesCreated - 1];
+        node.parent = (node.id % 2 == 1) ? (nodesCreated - 1) / 2 : nodesCreated / 2;
         node.left =
-            nodesNeeded > nodesCreated * 2 - 1 ? alphabet[nodesCreated * 2 - 1] : null;
+            nodesNeeded > nodesCreated * 2 - 1 ? nodesCreated * 2 - 1 : null;
         node.right =
-            nodesNeeded > nodesCreated * 2 ? alphabet[nodesCreated * 2] : null;
+            nodesNeeded > nodesCreated * 2 ? nodesCreated * 2 : null;
         nodesArray.push(node);
     }
 
---
+++ sandbox-site/src/routes/BracketNode.tsx
@@ -0,0 +1,105 @@
+import { stringify } from "querystring";
+import React from "react";
+import "./Bracket.scss";
+
+type BracketProps = {
+    id: number,
+    left: number,
+    right: number,
+    roundName: string,
+    leftEntrant: string,
+    rightEntrant: string
+}
+
+type BracketState = {
+
+}
+
+const alphabet = [
+    "Grand Finals",
+    "Semifinal A",
+    "Semifinal B",
+    "Quarterfinal A",
+    "Quarterfinal B",
+    "Quarterfinal C",
+    "Quarterfinal D",
+    "Play-in A",
+    "Play-in B",
+    "Play-in C",
+    "Play-in D",
+    "Play-in E",
+    "Play-in F",
+    "Play-in G",
+    "Play-in H",
+    "Play-in I",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+];
+
+export class BracketNode extends React.Component<BracketProps>{ // eslint-disable-line no-unused-vars
+    constructor(props: BracketProps) {
+        super(props);
+        this.state = {
+            winner: '',
+            leftEntrant: props.leftEntrant,
+            rightEntrant: props.rightEntrant
+        }
+    }
+    
+    
+    public render(): JSX.Element {
+        // let leftEntrant = this.state.leftEntrant;
+        // let rightEntrant = this.state.rightEntrant;
+        return (
+            <div key={this.props.id} className="bracket_node">
+                <div className="title">{this.props.roundName}</div>
+
+                {this.props.leftEntrant === '' ? <input onChange={(e)=> this.handleInput("left", e) }/> : <div>{ this.props.leftEntrant }</div>}
+                <button className="entrant_win_button" onClick={ ()=> this.leftWin(this.props) }>Winner</button>
+                <div className="node_seed">{this.props.left ? `Winner of ${ alphabet[this.props.left] }` : ' '}</div>
+
+                {this.props.rightEntrant === '' ? <input onChange={(e)=> this.handleInput("right", e) }/> : <div>{ this.props.rightEntrant }</div>}
+                <button className="entrant_win_button" onClick= { ()=> this.rightWin(this.props) }>Winner</button>
+                <div className="node_seed">{this.props.right ? `Winner of ${ alphabet[this.props.right] }` : ' '}</div>
+            </div>);
+    }
+    
+    handleInput = (side: string, e: React.FormEvent<HTMLInputElement>) => {
+        switch(side){
+            case "left":
+                this.setState({ leftEntrant: e.currentTarget.value !== null ? e.currentTarget.value : ""})
+                break;
+            case "right":
+                this.setState({ rightEntrant: e.currentTarget.value !== null ? e.currentTarget.value : ""})
+                break;
+        }
+    }
+
+    leftWin = (node: BracketProps) => {
+        this.setState({winner: node.leftEntrant});
+        // node.parent
+
+        // if node where parent id == node.parent 
+        // if this.node.id == parentNode.left { update node.leftEntrant to this.node.leftEntrant }
+        // else if this.node.id == parentNode.right { update node.leftEntrant to this.node.rightEntrant }
+    }
+
+    rightWin = (node: BracketProps) => {
+        this.setState({winner: node.rightEntrant});
+        // node.parent
+    
+        // if node where parent id == node.parent 
+        // if this.node.id == parentNode.right { update node.rightEntrant to this.node.rightEntrant }
+        // else if this.node.id == parentNode.left { update node.leftEntrant to this.node.rightEntrant }
+    }
+}
+
+export default BracketNode; // eslint-disable-line no-unused-vars
+
